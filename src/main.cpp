edb2d077886730fa675e9dfb75485d5cd4656843
vishnu s <vishnuskky2001@gmail.com>
2023-02-11 09:13:25 +0530

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/*
+# main.cpp
+
+ADVANCED SECURITY CONTROL ALGORITHM
+-----------------------------------
+
+[vishnus_technologies (C) 2023]
+[10-FEBRUARY-2023 09:11AM]
+-----------------------------------
+*/
+
+
 #include <Arduino.h>
 #include <SPI.h>
 #include <MFRC522.h> // library for MFRC522 RFID reader
@@ -24,19 +36,23 @@ RFID PIN CONFIGURATION
 #define CABIN_LIGHT_RPIN 4
 #define buzzerPin 7
 
+#define SWITCH_ON LOW
+#define SWITCH_OFF HIGH
+
 MFRC522 rfid(SS_PIN, RST_PIN);
 MFRC522::MIFARE_Key key;
 
 byte scanCardUID[4]; 
 bool authFlag = true;
 
+// AUTHENTICATION UIDs:
 /* SMPS authentication match UID: [int?= 16562146172] [hex?= A53E92AC] */
 const byte SMPSAuthUID[4] = {165, 62, 146, 172};
 
 /* Cabin light authenticaition UID: [int?= ] [hex?= ] */
 const byte ClightAuthUID[4] = {0xA, 0x3C, 0x90, 0xAF};
 
-int getID();
+int scanRFID();
 bool IsArrayEqual(const byte a1[], byte a2[], uint8_t asize);
 
 void setup() {
@@ -46,21 +62,22 @@ void setup() {
   pinMode(CABIN_LIGHT_RPIN, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
 
-  digitalWrite(SMPS_TRIGG_RPIN, LOW);
-  digitalWrite(CABIN_LIGHT_RPIN, LOW);
+  digitalWrite(SMPS_TRIGG_RPIN, SWITCH_OFF);
+  digitalWrite(CABIN_LIGHT_RPIN, SWITCH_OFF);
 
   noTone(buzzerPin);
   SPI.begin(); // start SPI communication.
   rfid.PCD_Init(); // initialize the MFRC522.
   delay(750);
-  Serial.println("Please scan your RFID Card at the scanner");
+  Serial.print("Please scan your RFID Card at the scanner");
 }
 
 void loop() {
-getID();
+scanRFID();
 }
 
-int getID() {
+// starts scanner
+int scanRFID() {
   if( ! rfid.PICC_IsNewCardPresent()) { 
     return 1; }
 
@@ -68,7 +85,7 @@ int getID() {
     return 1; }
        
   // Reads RFID UID and store
-  Serial.print("UID: ");
+  Serial.print("\nUID: ");
   for(int i = 0; i < rfid.uid.size; i++) {  
     scanCardUID[i] = rfid.uid.uidByte[i];
     //Serial.print(rfid.uid.uidByte[i]);
@@ -77,30 +94,32 @@ int getID() {
   }
   Serial.println();
 
-  /* COMPARES & CHECK AUTHENTICATION UIDs */
+  /* COMPARES & CHECK ALL AUTHENTICATION UIDs */
   // For SMPS power access authentication [switch ON]:
   if(IsArrayEqual(SMPSAuthUID, scanCardUID, rfid.uid.size)) {
-    if(digitalRead(SMPS_TRIGG_RPIN) == LOW) {
-      digitalWrite(SMPS_TRIGG_RPIN, HIGH);
+    if(digitalRead(SMPS_TRIGG_RPIN) == SWITCH_OFF) {
+      digitalWrite(SMPS_TRIGG_RPIN, SWITCH_ON);
       Serial.println("Access Granted: SMPS switched ON");
     }
-    else if(digitalRead(SMPS_TRIGG_RPIN) == HIGH) {
-      Serial.println("Access Granted: SMPS already switched ON!");
+    else if(digitalRead(SMPS_TRIGG_RPIN) == SWITCH_ON) {
+      digitalWrite(SMPS_TRIGG_RPIN, SWITCH_OFF);
+      Serial.println("[SMPS switched OFF]");
     }
   }
 
   // For cabin light power access [switch ON]: 
   if(IsArrayEqual(ClightAuthUID, scanCardUID, rfid.uid.size)) {
-    if(digitalRead(CABIN_LIGHT_RPIN) == LOW) {
-      digitalWrite(CABIN_LIGHT_RPIN, HIGH);
+    if(digitalRead(CABIN_LIGHT_RPIN) == SWITCH_OFF) {
+      digitalWrite(CABIN_LIGHT_RPIN, SWITCH_ON);
       Serial.println("Access Granted: Cabin light switched ON");
     }
-    else if(digitalRead(CABIN_LIGHT_RPIN) == HIGH) {
-      Serial.println("Access Granted: Cabin light already switched ON!");
+    else if(digitalRead(CABIN_LIGHT_RPIN) == SWITCH_ON) {
+      digitalWrite(CABIN_LIGHT_RPIN, SWITCH_OFF);
+      Serial.println("[Cabin light switched OFF]");
     }
   }
 
-  rfid.PICC_HaltA();     // halts PICC reading process.
+  rfid.PICC_HaltA();  // halts PICC reading process.
   return 0;
 }
 
